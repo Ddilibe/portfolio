3aa89e4ecf9c2501269f1ef173d63c13318d6082
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-08-31 15:32:45 +0100

    [Feat]: Hosting on railway

--- core/settings.py
+++ core/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRETKEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG', True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
