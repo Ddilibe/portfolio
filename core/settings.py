e0744e34d7430a40b8c14291b9937549e483d9c3
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-09-01 01:43:36 +0100

    [Feat]: Trying to host with railway

--- core/settings.py
+++ core/settings.py
@@ -51,7 +51,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -138,13 +137,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR,"static")
 ]
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR/ 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # STATIC_ROOT = "static/"
 
 # Default primary key field type
